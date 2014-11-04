ef5a0ff2f230a6e6d4dc64a9ce008f3b7446d0a0
Jérôme <jejefcgb@gmail.com>
2014-11-04 14:36:44 +0100

    plugins added

--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -1,4 +1,9 @@
 /home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes.dex : \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/randdusing/bluetoothle/BluetoothLePlugin.class \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/randdusing/bluetoothle/BluetoothLePlugin$2.class \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/randdusing/bluetoothle/BluetoothLePlugin$4.class \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/randdusing/bluetoothle/BluetoothLePlugin$1.class \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/randdusing/bluetoothle/BluetoothLePlugin$3.class \
 /home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/eirb/projets9/R$drawable.class \
 /home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/eirb/projets9/R$attr.class \
 /home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/eirb/projets9/R$xml.class \
@@ -6,4 +11,8 @@
 /home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/eirb/projets9/R$string.class \
 /home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/eirb/projets9/CordovaApp.class \
 /home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/com/eirb/projets9/R.class \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/org/apache/cordova/statusbar/StatusBar$3.class \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/org/apache/cordova/statusbar/StatusBar$2.class \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/org/apache/cordova/statusbar/StatusBar$1.class \
+/home/jduban/Dev/Android/projets9/platforms/android/ant-build/classes/org/apache/cordova/statusbar/StatusBar.class \
 /home/jduban/Dev/Android/projets9/platforms/android/ant-build/dexedLibs/classes-71afd1f3b7dcee3c7a4141cd2a1f9ba6.jar \
--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,26 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/com.randdusing.bluetoothle/www/bluetoothle.js",
+        "id": "com.randdusing.bluetoothle.BluetoothLe",
+        "clobbers": [
+            "window.bluetoothle"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.console": "0.2.11",
+    "org.apache.cordova.statusbar": "0.1.8",
+    "com.randdusing.bluetoothle": "1.0.6"
+}
 // BOTTOM OF METADATA
 });
