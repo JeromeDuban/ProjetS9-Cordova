1bb6f74600ca0c4f04579f27f91c0a1a903615dd
Jérôme <jejefcgb@gmail.com>
2014-11-04 14:39:39 +0100

    pluggins added.

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,109 @@
+// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
+(function () {
+
+  var scanTimer = null;
+
+  document.addEventListener('deviceready', function () {
+    console.log("OK good to go");
+
+    StatusBar.overlaysWebView( false );
+    StatusBar.backgroundColorByHexString('#ffffff');
+    StatusBar.styleDefault();
+
+    bluetoothle.initialize(initializeSuccess, initializeError);
+    
+    document.addEventListener('toggle', function (e) {
+      if ($(".toggle").hasClass("active")) {
+        console.log("Starting scan for beacons.");
+        var paramsObj = {"serviceUuids":[]};
+        bluetoothle.startScan(startScanSuccess, startScanError, paramsObj);
+      } else {
+        bluetoothle.stopScan(stopScanSuccess, stopScanError);
+        clearScanTimeout();
+        $(".beacon-entry").remove();
+      }
+    });
+  }, false);
+
+
+  function initializeSuccess(obj)
+  {
+    if (obj.status == "enabled")
+    {
+      console.log("Bluetooth initialized successfully, starting scan for beacons.");
+    }
+    else
+    {
+      console.log("Unexpected initialize status: " + obj.status);
+    }
+  }
+
+  function initializeError(obj)
+  {
+    console.log("Initialize error: " + obj.error + " - " + obj.message);
+  }
+
+  function startScanSuccess(obj)
+  {
+    if (obj.status == "scanResult")
+    {
+      console.log("Stopping scan..");
+      console.log("Beacon found : "+obj.address+" "+obj.name+" "+obj.rssi);
+
+      $(".table-view").append("<li class=\"table-view-cell beacon-entry\">Beacon "+obj.name+" : "+obj.address+" <br />RSSi : "+obj.rssi+"</li>");
+      // window.localStorage.setItem(addressKey, obj.address);
+      // connectDevice(obj.address);
+    }
+    else if (obj.status == "scanStarted")
+    {
+      console.log("Scan was started successfully, stopping in 10");
+      scanTimer = setTimeout(scanTimeout, 10000);
+    }
+    else
+    {
+      console.log("Unexpected start scan status: " + obj.status);
+    }
+  }
+
+  function startScanError(obj)
+  {
+    console.log("Start scan error: " + obj.error + " - " + obj.message);
+  }
+
+  function scanTimeout()
+  {
+    console.log("Scanning time out, stopping");
+    bluetoothle.stopScan(stopScanSuccess, stopScanError);
+  }
+
+  function clearScanTimeout()
+  { 
+      console.log("Clearing scanning timeout");
+    if (scanTimer != null)
+    {
+      clearTimeout(scanTimer);
+    }
+  }
+
+  function stopScanSuccess(obj)
+  {
+    if (obj.status == "scanStopped")
+    {
+      console.log("Scan was stopped successfully");
+    }
+    else
+    {
+      console.log("Unexpected stop scan status: " + obj.status);
+    }
+  }
+
+  function stopScanError(obj)
+  {
+    console.log("Stop scan error: " + obj.error + " - " + obj.message);
+  }
+
+
+
+
+
+}());
